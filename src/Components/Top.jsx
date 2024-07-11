03769e7c682b284ef0285e5342eca9e36c1f1978
kartik5026 <87748398+kartik5026@users.noreply.github.com>
2024-07-11 13:59:46 +0530

    Update Top.jsx

--- src/Components/Top.jsx
+++ src/Components/Top.jsx
@@ -1,6 +1,7 @@
 
 
 import "../index.css"
+import { Link } from "react-router-dom";
 function Top(){
     const url = "https://websitedemos.net/organic-shop-02/wp-content/uploads/sites/465/2021/03/organic-products-hero.png";
     return(
@@ -12,7 +13,7 @@ function Top(){
         <div className="item">
                 <h3>Best Quality Products</h3>
                 <h1>Join The Organic Movement!</h1>
-                <button><a href="/groceries">Shop Now</a></button>
+                <Link to="/groceries">Shop Now</Link>
 
             </div>
         </div>
